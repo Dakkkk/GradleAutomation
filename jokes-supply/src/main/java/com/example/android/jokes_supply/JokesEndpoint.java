ebabd5e144d34bf65be9fe96cfe01715b171c4e4
DaveKun <dawidkunicki88@gmail.com>
2017-06-04 19:48:31 +0200

    Defined Jokes-Supply webapp ( Project mode ) . Resolved Gradle findbugs:jsr305 conflict by adding code to app.build . Defined @Api and an endpoint method in JokesEndpoint in the supply library.

--- jokes-supply/src/main/java/com/example/android/jokes_supply/JokesEndpoint.java
+++ jokes-supply/src/main/java/com/example/android/jokes_supply/JokesEndpoint.java
@@ -1,10 +1,31 @@
 package com.example.android.jokes_supply;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+
 /**
  * Created by Dawid on 2017-06-04.
  */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "jokes-supply.gradleautomation.android.mobileallin.com",
+                ownerName = "jokes-supply.gradleautomation.android.mobileallin.com",
+                packagePath=""
+        )
+)
 
 public class JokesEndpoint {
 
+    /** Endpoint method that takes a name and displays welcome message */
+    @ApiMethod(name = "sayHi")
+    public MyData sayHi(@Named("name") String name) {
+        MyData response = new MyData();
+        response.setJokesData("Welcome, " + name + " have fun!");
 
+        return response;
+    }
 }
