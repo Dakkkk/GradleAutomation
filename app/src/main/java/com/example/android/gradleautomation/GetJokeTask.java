17503aa0b3a612d624298179112e76c2b45b14ca
DaveKun <dawidkunicki88@gmail.com>
2017-06-04 20:15:13 +0200

    Commented out tests

--- app/src/main/java/com/example/android/gradleautomation/GetJokeTask.java
+++ app/src/main/java/com/example/android/gradleautomation/GetJokeTask.java
@@ -2,7 +2,13 @@ package com.example.android.gradleautomation;
 
 import android.os.AsyncTask;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.mobileallin.android.gradleautomation.jokes_supply.myApi.MyApi;
+
+import java.io.IOException;
 
 /**
  * Created by Dawid on 2017-06-04.
@@ -29,6 +35,24 @@ public class GetJokeTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... params) {
+        if (myApiService == null) {
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?>
+                                                       abstractGoogleClientRequest)
+                                throws IOException {
+                            abstractGoogleClientRequest.setDisableGZipContent(true);
+                        }
+                    });
+            myApiService = builder.build();
+        }
+
         return null;
     }
 }
