4ce69042d92ec4cacf38eb7e8673fb0c18ea5ff6
DaveKun <dawidkunicki88@gmail.com>
2017-06-04 18:49:08 +0200

    Defined a simple Data class - model object for the endpoint. Defined emty JokesEndpoint class

---
+++ jokes-supply/src/main/java/com/example/android/jokes_supply/JokesEndpoint.java
@@ -0,0 +1,10 @@
+package com.example.android.jokes_supply;
+
+/**
+ * Created by Dawid on 2017-06-04.
+ */
+
+public class JokesEndpoint {
+
+
+}
---
+++ jokes-supply/src/main/java/com/example/android/jokes_supply/MyData.java
@@ -0,0 +1,20 @@
+package com.example.android.jokes_supply;
+
+/**
+ * Created by Dawid on 2017-06-04.
+ */
+
+/* Model for the endpoint data */
+
+public class MyData {
+
+    public String jokesData;
+
+    public String getJokesData() {
+        return jokesData;
+    }
+
+    public void setJokesData(String jokesData) {
+        this.jokesData = jokesData;
+    }
+}
