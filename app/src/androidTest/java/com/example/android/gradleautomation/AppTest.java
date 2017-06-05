ec8ff8846878156de1f888449c6a8c0d1b0327e7
DaveKun <dawidkunicki88@gmail.com>
2017-06-05 10:45:03 +0200

    Defined simple android test that checks if the joke was retrieved from backend and is visible in activity

--- app/src/androidTest/java/com/example/android/gradleautomation/AppTest.java
+++ app/src/androidTest/java/com/example/android/gradleautomation/AppTest.java
@@ -1,8 +1,14 @@
 package com.example.android.gradleautomation;
 
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
 /**
  * Created by Dawid on 2017-06-05.
  */
 
-public class AppTest {
+public class AppTest extends ApplicationTestCase<Application> {
+    public AppTest() {
+        super(Application.class);
+    }
 }
--- app/src/androidTest/java/com/example/android/gradleautomation/GetJokeTaskTest.java
+++ app/src/androidTest/java/com/example/android/gradleautomation/GetJokeTaskTest.java
@@ -1,8 +1,38 @@
 package com.example.android.gradleautomation;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
 /**
  * Created by Dawid on 2017-06-05.
  */
 
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class GetJokeTaskTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void changeText_sameActivity() {
+        onView(withId(R.id.button_tell_joke))
+                .perform(click());
+        onView(withId(R.id.text_view_joke))
+                .check(matches(not(withText(""))));
+    }
 }
+
